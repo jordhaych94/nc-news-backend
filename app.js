a6e507e226dd28656c984e777293e2ac895555a7
jordhaych94 <jordhaych94@gmail.com>
2026-02-23 10:36:09 +0000

    added CORS package and required the pkg

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const topicsRouter = require("./routes/topics.routes");
 const articlesRouter = require("./routes/articles.routes");
 const usersRouter = require("./routes/users.routes");
+const cors = require("cors");
+
+
 
 // making the server!
 const app = express();
-
+app.use(cors());
 //setup!
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // linking router (valid endpoints)
 app.use("/api/topics", topicsRouter);
