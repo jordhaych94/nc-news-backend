96e828f109d93ac5c70a7f30cd1351c643c21fbc
jordhaych94 <jordhaych94@gmail.com>
2026-02-24 14:38:01 +0000

    added img_url to coding topic

--- db/data/development-data/topics.js
+++ db/data/development-data/topics.js
@@ -3,7 +3,7 @@ module.exports = [
     description: "Code is love, code is life",
     slug: "coding",
     img_url:
-      "https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?_gl=1*5b6lde*_ga*MTQxOTY3NTY4Ni4xNzcxOTQyMzA2*_ga_8JE65Q40S6*czE3NzE5NDIzMDUkbzEkZzEkdDE3NzE5NDM0OTUkajU2JGwwJGgw",
+      "https://images.pexels.com/photos/6424586/pexels-photo-6424586.jpeg?w=700&h=700",
   },
   {
     description: "FOOTIE!",
